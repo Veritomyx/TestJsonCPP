5446b17ce939ea3752911d3ee37d3e353c21075c
Adam Tenderholt <adam_tenderholt@targetdiscovery.com>
2017-04-21 15:57:53 -0700

    Iterate through sub-items

--- main.cpp
+++ main.cpp
@@ -22,11 +22,24 @@ int main()
   Json::ValueConstIterator iter;
   for (iter = json->begin(); iter != json->end(); ++iter)
   {
-    std::cout << "..." << iter.key() << std::endl;
+    std::cout << "..." << iter.key() << ", value: " << *iter << std::endl;
+    Json::Value item = json->get(iter.key().asString(), Json::nullValue);
+    Json::ValueConstIterator item_iter;
+    for (item_iter = item.begin(); item_iter != item.end(); ++item_iter)
+    {
+      std::cout << "......" << item_iter.key() << ", value: " << *item_iter << std::endl;
+    }
   }
 
   std::cout << "Deleting json..." << std::endl;
   delete json;
+
+  Json::Value null = Json::nullValue;
+
+  std::cout << "Null attribtes:" << std::endl;
+  std::cout << "... size() - " << null.size() << std::endl;
+  std::cout << "... asInt() - " << null.asInt() << std::endl;
+  std::cout << ".... asString() - '" << null.asString() << "'" << std::endl;
 
   return 0;
 }
